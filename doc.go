9097f5b1dab4a27fe7df5215918a85fa26bed595
marwan-at-work <marwan.sameer@gmail.com>
2018-10-30 01:46:08 -0400

    add example to godoc

--- doc.go
+++ doc.go
@@ -1,2 +1,20 @@
-// Package fetch is a WIP Web Assembly fetch wrapper that avoids importing net/http.
+// Package fetch is a  Web Assembly fetch wrapper that avoids importing net/http.
+/*
+	package main
+
+	import (
+		"context"
+		"time"
+
+		"marwan.io/wasm-fetch"
+	)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	resp, err := fetch.Fetch("/some/api/call", &fetch.Opts{
+		Body:   strings.NewReader(`{"one": "two"}`),
+		Method: fetch.MethodPost,
+		Signal: ctx,
+	})
+*/
 package fetch // import "marwan.io/wasm-fetch"
